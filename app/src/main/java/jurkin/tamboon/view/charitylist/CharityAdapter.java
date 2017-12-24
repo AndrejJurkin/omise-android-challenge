8dc047c6af6d340f61df4bb5e10052a6fff76883
Andrej Jurkin <andrej.jurkin@gmail.com>
2017-12-24 12:50:55 +0100

    Implement CharityAdapter binding

--- app/src/main/java/jurkin/tamboon/view/charitylist/CharityAdapter.java
+++ app/src/main/java/jurkin/tamboon/view/charitylist/CharityAdapter.java
@@ -3,12 +3,21 @@ package jurkin.tamboon.view.charitylist;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jurkin.tamboon.R;
 import jurkin.tamboon.model.Charity;
 
 /**
@@ -30,12 +39,20 @@ public class CharityAdapter extends RecyclerView.Adapter<CharityAdapter.ViewHold
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_charity, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        Charity charity = data.get(position);
+        holder.charityName.setText(charity.getName());
 
+        Glide.with(holder.itemView.getContext())
+                .load(charity.getLogoUrl())
+                .into(holder.charityImage);
     }
 
     @Override
@@ -45,8 +62,18 @@ public class CharityAdapter extends RecyclerView.Adapter<CharityAdapter.ViewHold
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.charity_image)
+        ImageView charityImage;
+
+        @BindView(R.id.charity_name)
+        TextView charityName;
+
+        @BindView(R.id.donate_button)
+        Button donateButton;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
